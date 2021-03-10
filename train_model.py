76c33c99c9d5bbb1d427a4117190e6f21eef9766
ChaeLinYeo <evergreen786@hanyang.ac.kr>
2021-03-10 19:35:59 +0900

    오타수정..ㅠㅠ

--- train_model.py
+++ train_model.py
@@ -9,20 +9,33 @@ import os
 from detectron2.data import DatasetCatalog, MetadataCatalog
 # import PIL
 
-class Train_Data:
-    def train_model(metadata, model_dir, is_mask, num_workers, batch_size, max_iter, num_classes):
-        cfg = get_cfg()
-        cfg.merge_from_file(model_zoo.get_config_file(model_dir))
-        cfg.DATASETS.TRAIN = (metadata.name,)
-        cfg.DATASETS.TEST = ()   # no metrics implemented for this dataset
-        cfg.DATALOADER.NUM_WORKERS = num_workers
-        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model_dir)
-        cfg.MODEL.MASK_ON = is_mask
-        cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = batch_size
-        cfg.SOLVER.MAX_ITER = max_iter
-        cfg.MODEL.ROI_HEADS.NUM_CLASSES = num_classes
+"""
+함수 사용할 시
+cfg = train_model(metadata = mask_metadata,
+            model_dir= "Misc/cascade_mask_rcnn_R_50_FPN_3x.yaml",
+            is_mask= False,
+            num_workers= 4,
+            batch_size= 256,
+            max_iter= 200,
+            num_classes= 3)
+"""
 
-        os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-        trainer = DefaultTrainer(cfg) 
-        trainer.resume_or_load(resume=False)
-        trainer.train()
+def train_model(metadata, model_dir, is_mask, num_workers, batch_size, max_iter, num_classes):
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file(model_dir))
+    cfg.DATASETS.TRAIN = (metadata.name,)
+    cfg.DATASETS.TEST = ()   # no metrics implemented for this dataset
+    cfg.DATALOADER.NUM_WORKERS = num_workers
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model_dir)
+    cfg.MODEL.MASK_ON = is_mask
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = batch_size
+    cfg.SOLVER.IMS_PER_BATCH = 2
+    cfg.SOLVER.MAX_ITER = max_iter
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = num_classes
+
+    os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+    trainer = DefaultTrainer(cfg) 
+    trainer.resume_or_load(resume=False)
+    trainer.train()
+
+    return cfg
--- using_model.py
+++ using_model.py
@@ -6,21 +6,22 @@
 from detectron2.utils.visualizer import ColorMode
 import glob
 import cv2
+import matplotlib.pyplot as plt
 
 # inference
 def using_model(metadata, threshold):
-    cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, 'model_final.pth')
+    cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, 'model_final.pth') # 나중에 용주님 토치서브로 넘길것
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = threshold   # set the testing threshold for this model
     cfg.DATASETS.TEST = (metadata.name, )
     predictor = DefaultPredictor(cfg)
 
-    images = [cv2.imread(file) for file in glob.glob("이미지경로/*.png")]
+    images = [cv2.imread(file) for file in glob.glob("/gdrive/MyDrive/img/*.png")] # 이미지 경로, 확장자 수정
 
     dataset_dicts = DatasetCatalog.get(metadata.name)
     for i in range(len(images)):
         im = images[i]
         outputs = predictor(im)
-        v = Visualizer(im[:, :, ::-1], metadata=chess_metadata, scale=0.8)
+        v = Visualizer(im[:, :, ::-1], metadata=metadata, scale=0.8)
         v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
         plt.figure(figsize = (14, 10))
         plt.imshow(cv2.cvtColor(v.get_image()[:, :, ::-1], cv2.COLOR_BGR2RGB))
